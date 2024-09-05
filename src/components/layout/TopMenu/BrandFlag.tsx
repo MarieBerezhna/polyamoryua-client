a323cbe15ae23f657dca884410c9021a438f9b17
Marie Berezhna <marie.berezhna@gmail.com>
2024-09-05 19:24:41 +0200

    add alt to brand image

--- src/components/layout/TopMenu/BrandFlag.tsx
+++ src/components/layout/TopMenu/BrandFlag.tsx
@@ -1,5 +1,5 @@
 const BrandFlag = ({ width = 135, height = 77 }) => (
-	<img src="images/polyprideflag.webp" width={width} height={height} />
+	<img src="images/polyprideflag.webp" width={width} height={height} alt="прапор поліаморії" />
 );
 
 export default BrandFlag;
